64dcbab3689869b4d08124937a1b47ebdc0e2c29
Jeffrey Rodrigues <jeffrey.rodriigues@gmail.com>
2023-09-19 20:00:15 -0300

    Add Projeto Maven

---
+++ src/main/java/com/matera/bootcampmatera/BootcampMatera2023SetembroApplication.java
@@ -0,0 +1,13 @@
+package com.matera.bootcampmatera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampMatera2023SetembroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampMatera2023SetembroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matera/bootcampmatera/BootcampMatera2023SetembroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.bootcampmatera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampMatera2023SetembroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
